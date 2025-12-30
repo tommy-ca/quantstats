7e0929546e3b7bf044dd28b46bcc275cf8cbf11c
Tommy K <140900186+tommy-ca@users.noreply.github.com>
2025-12-30 03:49:57 +0100

    chore(deps): Add beautifulsoup4 for testing

--- tests/test_requirements.py
+++ tests/test_requirements.py
@@ -6,3 +6,10 @@ def test_requirements_contains_hvplot():
         requirements = f.read()
     
     assert "hvplot" in requirements, "hvplot should be in requirements.txt"
+
+def test_requirements_contains_bs4():
+    req_file = "requirements.txt"
+    with open(req_file, "r") as f:
+        requirements = f.read()
+    
+    assert "beautifulsoup4" in requirements, "beautifulsoup4 should be in requirements.txt"
